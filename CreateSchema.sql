b07847bf205a11e5244776ee823fb5e24e9be1cd
Paulo Ferreira <pauloferreira.vnc@gmail.com>
2023-05-27 20:18:02 +0100

    ficheiro de criacao e povoamento adicionados

--- CreateSchema.sql
+++ CreateSchema.sql
@@ -1,8 +1,20 @@
+-- ------------------------------------------------------
+-- ------------------------------------------------------
+-- Universidade do Minho
+-- Licenciatura em Engenharia Informática
+-- Unidade Curricular de Bases de Dados
+-- 
+-- Caso de Estudo: A loja da Dona Lola
+-- Criação do Esquema Físico
+-- Ano letivo 22/23
+-- ------------------------------------------------------
+-- ------------------------------------------------------
+
 -- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION,TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
 -- Schema mydb
@@ -11,60 +23,64 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `mydb` ;
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Marca`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`Marca` (
-  `nome` VARCHAR(75) NOT NULL,
-  `país_Origem` VARCHAR(75) NOT NULL,
-  `descrição` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`nome`));
+  `Nome` VARCHAR(75) NOT NULL,
+  `Pais_Origem` VARCHAR(75) NOT NULL,
+  `Descricao` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`nome`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Coleção`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Coleção` (
-  `id` INT NOT NULL,
-  `nome` VARCHAR(75) NOT NULL,
-  `ano` DATE NOT NULL,
-  `descrição` VARCHAR(255) NOT NULL,
-  `Estação` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE IF NOT EXISTS `mydb`.`Colecao` (
+  `Id` INT NOT NULL,
+  `Nome` VARCHAR(75) NOT NULL,
+  `Ano` DATE NOT NULL,
+  `Descricao` VARCHAR(255) NOT NULL,
+  `Estacao` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`Peça de Roupa`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Peça de Roupa` (
-  `id` INT NOT NULL,
-  `nome` VARCHAR(75) NOT NULL,
-  `gênero` VARCHAR(20) NOT NULL,
-  `tamanho` VARCHAR(10) NOT NULL,
-  `stock` INT NOT NULL,
-  `cor` VARCHAR(45) NOT NULL,
-  `descrição` VARCHAR(255) NOT NULL,
-  `tipo` VARCHAR(45) NOT NULL,
-  `preçoVenda` DECIMAL(8,2) NOT NULL,
-  `Promoção` INT NULL,
+CREATE TABLE IF NOT EXISTS `mydb`.`PecaDRoupa` (
+  `Id` INT NOT NULL,
+  `Nome` VARCHAR(75) NOT NULL,
+  `Genero` VARCHAR(20) NOT NULL,
+  `Tamanho` VARCHAR(10) NOT NULL,
+  `Stock` INT NOT NULL,
+  `Cor` VARCHAR(45) NOT NULL,
+  `Descricao` VARCHAR(255) NOT NULL,
+  `Tipo` VARCHAR(45) NOT NULL,
+  `PrecoVenda` DECIMAL(8,2) NOT NULL,
+  `Promocao` INT NULL,
   `Marca_nome` VARCHAR(75) NOT NULL,
-  `Coleção_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `Marca_nome`, `Coleção_id`),
-  INDEX `fk_Peça de Roupa_Marca_idx` (`Marca_nome` ASC) VISIBLE,
-  INDEX `fk_Peça de Roupa_Coleção1_idx` (`Coleção_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Peça de Roupa_Marca`
+  `Colecao_id` INT NULL,
+  PRIMARY KEY (`Id`, `Marca_nome`),
+  INDEX `fk_PecaDRoupa_Marca_idx` (`Marca_nome` ASC) VISIBLE,
+  INDEX `fk_PecaDRoupa_Colecao1_idx` (`Colecao_id` ASC) VISIBLE,
+  CONSTRAINT `fk_PecaDRoupa_Marca`
     FOREIGN KEY (`Marca_nome`)
-    REFERENCES `mydb`.`Marca` (`nome`)
+    REFERENCES `mydb`.`Marca` (`Nome`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Peça de Roupa_Coleção1`
-    FOREIGN KEY (`Coleção_id`)
-    REFERENCES `mydb`.`Coleção` (`id`)
+  CONSTRAINT `fk_PecaDRoupa_Colecao1`
+    FOREIGN KEY (`Colecao_id`)
+    REFERENCES `mydb`.`Colecao` (`Id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION
+) ENGINE = InnoDB;
+
 
 
 -- -----------------------------------------------------
@@ -72,64 +88,71 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Peça de Roupa` (
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`Fornecedor` (
   `NIF` INT NOT NULL,
-  `nome` VARCHAR(75) NOT NULL,
-  `rua` VARCHAR(100) NOT NULL,
-  `email` VARCHAR(75) NOT NULL,
-  `telefone` INT NOT NULL,
+  `Nome` VARCHAR(75) NOT NULL,
+  `Rua` VARCHAR(100) NOT NULL,
+  `Email` VARCHAR(75) NOT NULL,
+  `Telefone` INT NOT NULL,
   `Localidade` VARCHAR(100) NOT NULL,
   `CodPost` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`NIF`));
+  PRIMARY KEY (`NIF`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`RoupaFornecedor`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`RoupaFornecedor` (
-  `Peça de Roupa_id` INT NOT NULL,
+  `PecaDRoupa_id` INT NOT NULL,
   `Roupa_Marca_nome` VARCHAR(75) NOT NULL,
   `Fornecedor_NIF` INT NOT NULL,
-  `PreçoComp` DECIMAL(8) NOT NULL,
+  `PrecoComp` DECIMAL(8) NOT NULL,
   `DataFornecimento` DATE NOT NULL,
   `Quantidade` INT NOT NULL,
-  PRIMARY KEY (`Peça de Roupa_id`, `Roupa_Marca_nome`, `Fornecedor_NIF`),
-  INDEX `fk_Peça de Roupa_has_Fornecedor_Fornecedor1_idx` (`Fornecedor_NIF` ASC) VISIBLE,
-  INDEX `fk_Peça de Roupa_has_Fornecedor_Peça de Roupa1_idx` (`Peça de Roupa_id` ASC, `Roupa_Marca_nome` ASC) VISIBLE,
-  CONSTRAINT `fk_Peça de Roupa_has_Fornecedor_Peça de Roupa1`
-    FOREIGN KEY (`Peça de Roupa_id` , `Roupa_Marca_nome`)
-    REFERENCES `mydb`.`Peça de Roupa` (`id` , `Marca_nome`)
+  PRIMARY KEY (`PecaDRoupa_id`, `Roupa_Marca_nome`, `Fornecedor_NIF`),
+  INDEX `fk_PecaDRoupa_has_Fornecedor_Fornecedor1_idx` (`Fornecedor_NIF` ASC) VISIBLE,
+  INDEX `fk_PecaDRoupa_has_Fornecedor_PecaDRoupa1_idx` (`PecaDRoupa_id` ASC, `Roupa_Marca_nome` ASC) VISIBLE,
+  CONSTRAINT `fk_PecaDRoupa_has_Fornecedor_PecaDRoupa1`
+    FOREIGN KEY (`PecaDRoupa_id` , `Roupa_Marca_nome`)
+    REFERENCES `mydb`.`PecaDRoupa` (`id` , `Marca_nome`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Peça de Roupa_has_Fornecedor_Fornecedor1`
+  CONSTRAINT `fk_PecaDRoupa_has_Fornecedor_Fornecedor1`
     FOREIGN KEY (`Fornecedor_NIF`)
     REFERENCES `mydb`.`Fornecedor` (`NIF`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `mydb`.`ColeçãoFornecedor`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`ColeçãoFornecedor` (
-  `Coleção_id` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `mydb`.`ColecaoFornecedor` (
+  `Colecao_id` INT NOT NULL,
   `Fornecedor_NIF` INT NOT NULL,
-  `PreçoComp` DECIMAL(8,2) NOT NULL,
+  `PrecoComp` DECIMAL(8,2) NOT NULL,
   `DataFornecimento` DATE NOT NULL,
   `Quantidade` INT NOT NULL,
-  PRIMARY KEY (`Coleção_id`, `Fornecedor_NIF`),
-  INDEX `fk_Coleção_has_Fornecedor_Fornecedor1_idx` (`Fornecedor_NIF` ASC) VISIBLE,
-  INDEX `fk_Coleção_has_Fornecedor_Coleção1_idx` (`Coleção_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Coleção_has_Fornecedor_Coleção1`
-    FOREIGN KEY (`Coleção_id`)
-    REFERENCES `mydb`.`Coleção` (`id`)
+  PRIMARY KEY (`Colecao_id`, `Fornecedor_NIF`),
+  INDEX `fk_Colecao_has_Fornecedor_Fornecedor1_idx` (`Fornecedor_NIF` ASC) VISIBLE,
+  INDEX `fk_Colecao_has_Fornecedor_Colecao1_idx` (`Colecao_id` ASC) VISIBLE,
+  CONSTRAINT `fk_Colecao_has_Fornecedor_Colecao1`
+    FOREIGN KEY (`Colecao_id`)
+    REFERENCES `mydb`.`Colecao` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Coleção_has_Fornecedor_Fornecedor1`
+  CONSTRAINT `fk_Colecao_has_Fornecedor_Fornecedor1`
     FOREIGN KEY (`Fornecedor_NIF`)
     REFERENCES `mydb`.`Fornecedor` (`NIF`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION)
+  ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- ------------------------------------------------------
+-- <fim>
+-- ------------------------------------------------------

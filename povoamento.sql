9b4d7d2d67ff41f733647573359ab3d946350cfd
Paulo Ferreira <pauloferreira.vnc@gmail.com>
2023-06-05 16:43:57 +0100

    novos procedures and queries added

--- povoamento.sql
+++ povoamento.sql
@@ -1,214 +1,3 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
-
--- -----------------------------------------------------
--- Table `mydb`.`Genero`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Genero` (
-  `genero_id` INT NOT NULL,
-  `genero_descricao` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`genero_id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`Tipo`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Tipo` (
-  `tipo_id` INT NOT NULL,
-  `tipo_descricao` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`tipo_id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`Marca`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Marca` (
-  `marca_id` INT NOT NULL,
-  `marca_descricao` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`marca_id`));
-
-
--- -----------------------------------------------------
--- Table `mydb`.`PecaDeRoupa`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`PecaDeRoupa` (
-  `peca_id` INT NOT NULL,
-  `peca_nome` VARCHAR(75) NOT NULL,
-  `tamanho` VARCHAR(10) NOT NULL,
-  `stock` INT NOT NULL,
-  `cor` VARCHAR(45) NOT NULL,
-  `peca_descricao` VARCHAR(255) NOT NULL,
-  `precovenda` DECIMAL(8,2) NOT NULL,
-  `promocao` INT NULL,
-  `genero_id` INT NOT NULL,
-  `tipo_id` INT NOT NULL,
-  `marca_id` INT NOT NULL,
-  PRIMARY KEY (`peca_id`),
-  INDEX `idGeneroFK_idx` (`genero_id` ASC) VISIBLE,
-  INDEX `idTipoFK_idx` (`tipo_id` ASC) VISIBLE,
-  INDEX `idMarcaFK_idx` (`marca_id` ASC) VISIBLE,
-  CONSTRAINT `idGeneroFK`
-    FOREIGN KEY (`genero_id`)
-    REFERENCES `mydb`.`Genero` (`genero_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `idTipoFK`
-    FOREIGN KEY (`tipo_id`)
-    REFERENCES `mydb`.`Tipo` (`tipo_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `idMarcaFK`
-    FOREIGN KEY (`marca_id`)
-    REFERENCES `mydb`.`Marca` (`marca_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-
--- -----------------------------------------------------
--- Table `mydb`.`Fornecedor`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Fornecedor` (
-  `NIF` INT NOT NULL,
-  `for_nome` VARCHAR(75) NOT NULL,
-  `for_rua` VARCHAR(100) NOT NULL,
-  `for_localidade` VARCHAR(100) NOT NULL,
-  `for_codigopostal` VARCHAR(45) NOT NULL,
-  `for_email` VARCHAR(75) NOT NULL,
-  PRIMARY KEY (`NIF`));
-
-
--- -----------------------------------------------------
--- Table `mydb`.`Colecao`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Colecao` (
-  `colecao_id` INT NOT NULL,
-  `colecao_nome` VARCHAR(75) NOT NULL,
-... (116 linhas)
-Recolher
-criacaoDB.sql
-8 KB
-sleep — Hoje às 18:17
-povoamento 
--- ------------------------------------------------------
--- ------------------------------------------------------
--- Universidade do Minho
--- Licenciatura em Engenharia Informática
--- Unidade Curricular de Bases de Dados
--- 
--- Caso de Estudo: A loja da Dona Lola
--- Criação do Esquema Físico
--- Ano letivo 22/23
--- ------------------------------------------------------
--- ------------------------------------------------------
-
---
--- Esquema: "Loja D.Lola"
-USE `mydb` ;
-
---
--- Permissão para fazer operações de remoção de dados.
-SET SQL_SAFE_UPDATES = 0;
-
---
--- Povoamento da tabela "Genero"
-INSERT INTO genero
-	(genero_id, genero_descricao)
-	VALUES 
-		(1, 'Masculino'),
-        (2, 'Feminino'),
-        (3, 'Unissexo')
-        ;
---
--- DELETE FROM genero;
--- SELECT * FROM genero;
-
---
--- Povoamento da tabela "Tipo"
-INSERT INTO tipo
-	(tipo_id, tipo_descricao)
-	VALUES 
-		(1, 'Camiseta'),
-        (2, 'Calça'),
-        (3, 'Blazer'),
-        (4, 'Vestido'),
-        (5, 'Moletom'),
-        (6, 'Jaqueta'),
-        (7, 'Bermuda'),
-        (8, 'Camisa')
-        ;
---
--- DELETE FROM tipo;
--- SELECT * FROM tipo;
-
---
--- Povoamento da tabela "Marca"
-INSERT INTO marca
-	(marca_id, marca_descricao)
-	VALUES 
-		(1, 'Zara'),
-        (2, 'H&M'),
-        (3, 'Gucci'),
-        (4, 'Chanel'),
-        (5, 'Nike')
-        ;
---
--- DELETE FROM marca;
--- SELECT * FROM marca;
-
---
--- Povoamento da tabela "colecao"
-INSERT INTO colecao
-	(colecao_id, colecao_nome, ano, colecao_descricao, estacao)
-	VALUES 
-		(1, 'Primavera/Verão 2023', '2023-01-01', 'Coleção inspirada nas cores vibrantes e estampas florais da temporada primavera/verão.', 'Primavera/Verão'),
-		(2, 'Outono/Inverno 2023', '2023-01-01', 'Coleção com peças quentes e elegantes, ideais para os meses mais frios do outono/inverno.', 'Outono/Inverno'),
-		(3, 'Verão 2022', '2022-01-01', 'Coleção com roupas leves e descontraídas para aproveitar os dias quentes de verão.', 'Verão'),
-		(4, 'Inverno 2022', '2022-01-01', 'Coleção com roupas confortáveis e aconchegantes para enfrentar as baixas temperaturas do inverno.', 'Inverno'),
-		(5, 'Primavera 2021', '2021-01-01', 'Coleção com peças frescas e estampas florais para celebrar a chegada da primavera.', 'Primavera')
-	;
---
--- DELETE FROM colecao;
--- SELECT * FROM colecao;
-
---
--- Povoamento da tabela "colecaofornecedor"
-INSERT INTO colecaofornecedor 
-	(precocompra, datafornecimento, pecaColFor_quantidade, peca_id, for_NIF,colecao_id)
-	VALUES
-		(29.99, '2023-05-15', 50, 1, 10549492,1),
-		(35.99, '2023-05-16', 40, 2, 51916191,1),
-		(79.99, '2023-05-17', 20, 3, 51679649,2),
-		(49.99, '2023-05-18', 30, 4, 15479563,2),
-		(59.99, '2023-05-19', 25, 5, 79836214,2)
-	;
---
--- DELETE FROM colecaofornecedor;
--- SELECT * FROM colecaofornecedor;
-
---
--- Povoamento da tabela "fornecedor"
-INSERT INTO fornecedor 
-	(NIF, for_nome, for_rua, for_email, for_Localidade, for_codigopostal)
-... (114 linhas)
-Recolher
-povoamento.sql
-8 KB
-﻿
 -- ------------------------------------------------------
 -- ------------------------------------------------------
 -- Universidade do Minho
@@ -322,21 +111,6 @@ INSERT INTO fornecedor
 -- SELECT * FROM fornecedor;
 
 --
--- Povoamento da tabela "marca"
-INSERT INTO marca 
-	(marca_id, marca_descricao)
-	VALUES 
-		('Zara', 'Marca global de moda conhecida por suas tendências e preços acessíveis.'),
-		('Gucci', 'Marca de luxo que oferece roupas, acessórios e artigos de couro sofisticados.'),
-		('Nike', 'Marca líder em sportswear, oferecendo roupas esportivas e calçados inovadores.'),
-		('H&M', 'Marca de moda acessível com uma ampla gama de roupas para mulheres, homens e crianças.'),
-		('Chanel', 'Marca icônica de moda, perfumes e acessórios de luxo, conhecida por seu estilo clássico e elegante.')
-	;
---
--- DELETE FROM marca;
--- SELECT * FROM marca;
-
---
 -- Povoamento da tabela "PecaDRoupa"
 INSERT INTO PecaDeRoupa 
 	(peca_id, peca_nome, tamanho, stock, cor, peca_descricao, precovenda, promocao, genero_id, tipo_id, marca_id)
@@ -374,45 +148,43 @@ INSERT INTO PecaFornecedor
 -- SELECT * FROM PecaFornecedor;
 
 --
--- Povoamento da tabela "Localizacao"
+-- Povoamento da tabela "localizacao"
 INSERT INTO localizacao 
-	(loc_id, loc_descricao, loc_rua, loc_localizacao, loc_codigopostal,loc_telefone)
+	(loc_id, loc_descricao, loc_rua, loc_localizacao, loc_codigopostal, loc_telefone)
 	VALUES
-		(1, NULL, NULL, NULL, NULL, NULL),
-		(2, NULL, NULL, NULL, NULL, NULL),
-		(3, NULL, NULL, NULL, NULL, NULL),
-		(4, NULL, NULL, NULL, NULL, NULL),
-		(5, NULL, NULL, NULL, NULL, NULL)
+		(1, 'Loja de Roupas A', 'Rua Principal, 123', 'Cidade X', '12345-678', '157984369'),
+		(2, 'Loja de Roupas B', 'Avenida Central, 456', 'Cidade Y', '23456-789', '965874238'),
+		(3, 'Loja de Roupas C', 'Praça das Flores, 789', 'Cidade Z', '34567-890', '968741258'),
+		(4, 'Loja de Roupas D', 'Rua Secundária, 456', 'Cidade W', '45678-901', '968713584'),
+		(5, 'Loja de Roupas E', 'Avenida Principal, 789', 'Cidade V', '56789-012', '968748789')
 	;
 --
 -- DELETE FROM localizacao;
 -- SELECT * FROM localizacao;
 
---
--- Povoamento da tabela "Contacto"
+-- Povoamento da tabela "contacto"
 INSERT INTO contacto 
 	(id, telefone, idFornecedor)
 	VALUES
-		(1, NULL, NULL),
-		(2, NULL, NULL),
-		(3, NULL, NULL),
-		(4, NULL, NULL),
-		(5, NULL, NULL)
+		(1, '965874582', 1),
+		(2, '854712369', 2),
+		(3, '789534871', 3),
+		(4, '897465831', 4),
+		(5, '458712589', 5)
 	;
 --
 -- DELETE FROM contacto;
 -- SELECT * FROM contacto;
 
---
--- Povoamento da tabela "RoupaLocalizacao"
+-- Povoamento da tabela "roupaLocalizacao"
 INSERT INTO roupaLocalizacao 
 	(pecaLoc_quantidade, peca_id, loc_id)
 	VALUES
-		(1, NULL, NULL),
-		(2, NULL, NULL),
-		(3, NULL, NULL),
-		(4, NULL, NULL),
-		(5, NULL, NULL)
+		(1, 1, 1),
+		(2, 2, 2),
+		(3, 3, 3),
+		(4, 4, 4),
+		(5, 5, 5)
 	;
 --
 -- DELETE FROM roupaLocalizacao;
